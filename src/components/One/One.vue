30dafe92e983cd08abf4d500ac422098c773a890
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2018-04-09 17:11:25 +0800

    feat:信息广播

--- src/components/One/One.vue
+++ src/components/One/One.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="one">
+        <div class="logo ani" swiper-animate-effect="fadeInDown" swiper-animate-duration="1s" swiper-animate-delay="0.2s"></div>            
+        <div class="bt-1 ani" swiper-animate-effect="fadeInLeft" swiper-animate-duration="1s" swiper-animate-delay="1.25s"></div>
+        <div class="bt-2 ani" swiper-animate-effect="fadeInRight" swiper-animate-duration="1s" swiper-animate-delay="0.75s"></div>
+        <div class="bt-3 ani" swiper-animate-effect="fadeInUp" swiper-animate-duration="1s" swiper-animate-delay="1.5s"></div>
+        <div class="bt-4 ani" swiper-animate-effect="fadeInRight" swiper-animate-duration="1s" swiper-animate-delay="1.5s"></div>
         <div class="from">
-            <div class="logo ani" swiper-animate-effect="fadeInDown" swiper-animate-duration="1s" swiper-animate-delay="0.2s"></div>            
-            <div class="bt-1 ani" swiper-animate-effect="fadeInLeft" swiper-animate-duration="1s" swiper-animate-delay="1s"></div>
-            <div class="bt-2 ani" swiper-animate-effect="fadeInRight" swiper-animate-duration="1s" swiper-animate-delay="1.25s"></div>
-            <div class="bt-3 ani" swiper-animate-effect="fadeInLeft" swiper-animate-duration="1s" swiper-animate-delay="1.5s"></div>
-            <div class="bt-4 ani" swiper-animate-effect="fadeInRight" swiper-animate-duration="1s" swiper-animate-delay="1.75s"></div>
             <div class="text ani" swiper-animate-effect="fadeInDown" swiper-animate-duration="1s" swiper-animate-delay="0.5s">河南建业艾美酒店</div>
             <div class="text-1 ani" swiper-animate-effect="fadeInDown" swiper-animate-duration="1s" swiper-animate-delay="1s">
                 关注魅力881官方微信，申请电子卡券，凭卡券用餐尊享魅力881粉丝福利折扣。
@@ -13,8 +13,8 @@
             <div class="text-2 ani" swiper-animate-effect="fadeInDown" swiper-animate-duration="1s" swiper-animate-delay="1.5s">
                 截至4月30日 尊享6.6折！
             </div>
-            <img src="./hotel.gif" alt="" class="hotel ani" swiper-animate-effect="fadeIn" swiper-animate-duration="1s" swiper-animate-delay="1.75s">
-            <img src="./qrcode-channel.png" alt="" class="qrcode ani" swiper-animate-effect="bounceIn" swiper-animate-duration="1s" swiper-animate-delay="2.25s">
+            <img src="" alt="" class="hotel ani" swiper-animate-effect="fadeIn" swiper-animate-duration="1s" swiper-animate-delay="1.75s">
+            <img src="" alt="" class="qrcode ani" swiper-animate-effect="bounceIn" swiper-animate-duration="1s" swiper-animate-delay="2.25s">
         </div>                
        
     </div>
@@ -38,7 +38,8 @@ export default {
 .one
     width 100%
     height 100%
-    background-color #121532
+    background url('../common/bg.jpg') center center no-repeat
+    background-size cover
     .text
         position absolute
         top 200px
@@ -84,40 +85,41 @@ export default {
         left 0
         width 100%
         height 120px
-        background url('./icon-hndt-channel.png') center center no-repeat
-        background-size contain    
+        // background url('./icon-hndt-channel.png') center center no-repeat
+        // background-size contain    
     .bt-1
         position absolute
         bottom 55px
         left 0px
-        width 295px
-        height 241px        
-        background url('./bt-1.png') center center no-repeat
+        width 640px
+        height 398px 
+        background url('../common/left.png') center center no-repeat
         background-size cover
     .bt-2
         position absolute
         bottom 72px
         right 0px
-        width 345px
-        height 224px        
-        background url('./bt-2.png') center center no-repeat
+        width 482px
+        height 179px        
+        background url('../common/right.png') center center no-repeat
         background-size cover
     .bt-3
         position absolute
         bottom 0px
-        left 0px
-        width 375px
-        height 139px        
-        background url('./bt-3.png') center center no-repeat
+        right 0px
+        width 335px
+        height 486px        
+        background url('../common/up.png') center center no-repeat
         background-size cover
     .bt-4
         position absolute
-        bottom 0px
+        top 40px
         right 0px
-        width 492px
-        height 217px        
-        background url('./bt-4.png') center center no-repeat
+        width 400px
+        height 323px        
+        background url('../common/top.png') center center no-repeat
         background-size cover
+    
     
 </style>
 
