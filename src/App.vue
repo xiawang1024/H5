f2c4bd5e59c9af5ccb72dd6bc362a85fb69ca50f
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2018-04-10 16:25:35 +0800

    feat:增加audio和uparrow组件

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">    
+    <wx-audio></wx-audio>
     <router-view/>
+    <up-arrow></up-arrow>
   </div>
 </template>
 
 <script>
+import Audio from 'components/Audio/Audio'
+import UpArrow from 'components/UpArrow/UpArrow'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    'wx-audio':Audio,
+    UpArrow
+  }
 }
 </script>
 
