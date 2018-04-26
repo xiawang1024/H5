7086dfc1a3d06e277efcbf4bb90eaefa8a8d81e6
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2018-04-26 11:58:46 +0800

    报名字段

--- src/api/index.js
+++ src/api/index.js
@@ -1,21 +1,18 @@
-import axios from 'axios'
+import axios from 'axios';
 
 // axios.defaults.baseURL = '/api';
 axios.defaults.baseURL = 'http://a.weixin.hndt.com';
-const postUserInfo = (name, mobile, work, age, personNum, origin, openId) => axios.post('/user/add/hn', {
-    name,
-    mobile, 
-    work,  
-    age,
-    personNum,
-    origin,
-    openId
-})
+const postUserInfo = (name, mobile, job, age, personCount, origin, openId) =>
+	axios.post('/user/add/hn', {
+		name,
+		mobile,
+		job,
+		age,
+		personCount,
+		origin,
+		openId
+	});
 
-const checkOpenId = (openId, origin) => axios.get('/user/check/?openid=' + openId + '&origin=' + origin)
+const checkOpenId = (openId, origin) => axios.get('/user/check/?openid=' + openId + '&origin=' + origin);
 
-
-export {
-    postUserInfo,
-    checkOpenId
-}
+export { postUserInfo, checkOpenId };
