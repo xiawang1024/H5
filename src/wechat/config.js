1e3d68b7114f61d8f247dddb4d7db50e124ee103
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2018-06-06 16:18:03 +0800

    feat:百度统计

--- src/wechat/config.js
+++ src/wechat/config.js
@@ -4,32 +4,22 @@
  * DEFAULT_IMGURL String 分享图标链接
  * DEFAULT_DESC String 分享描述
  */
-let title = ''
-let link = ''
-let imgUrl = ''
-let desc = ''
+let title = '';
+let link = '';
+let imgUrl = '';
+let desc = '';
 
-import { getChannelItem } from 'api/index'
-import channelInfo from '../config'
+import { getChannelItem } from 'api/index';
+import channelInfo from '../config';
 
-document.title = channelInfo.title
+document.title = channelInfo.title;
 
 getChannelItem(channelInfo.cid).then((res) => {
-    let data = res.data
-    title = data.name;
-    link= channelInfo.url;
-    imgUrl = `http://program.hndt.com${data.image}`;
-    desc = data.description;
-})
+	let data = res.data;
+	title = data.name;
+	link = channelInfo.url;
+	imgUrl = `http://program.hndt.com${data.image}`;
+	desc = data.description;
+});
 
-export {
-    title,
-    link,
-    imgUrl,
-    desc
-} 
-
-
-
-
-
+export { title, link, imgUrl, desc };
