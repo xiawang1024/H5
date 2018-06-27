bdb1e82fa8f642f033530da5c87d4f36bc264b9a
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2018-06-27 16:21:34 +0800

    横屏提示

--- src/main.js
+++ src/main.js
@@ -1,32 +1,42 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import App from './App';
 // import router from 'router'
-import 'base/stylus/index.styl'
+import 'base/stylus/index.styl';
 
-import 'base/stylus/animate.css'
+import 'base/stylus/animate.css';
 
-/* 微信自定义分享 */ 
-import 'wechat/index.js'
+/* 微信自定义分享 */
 
-/* swiper插件 */ 
-import VueAwesomeSwiper　from 'vue-awesome-swiper'
-import 'animate.css'
-import 'swiper/dist/css/swiper.css'
-Vue.use(VueAwesomeSwiper)
+import 'wechat/index.js';
+
+/* swiper插件 */
+
+import VueAwesomeSwiper from 'vue-awesome-swiper';
+import 'animate.css';
+import 'swiper/dist/css/swiper.css';
+Vue.use(VueAwesomeSwiper);
 
 /* fastclick修复移动端点击延迟300ms */
-import fastclick from 'fastclick'
-fastclick.attach(document.body)
+import fastclick from 'fastclick';
+fastclick.attach(document.body);
 
+function screen() {
+	if (window.orientation == 90 || window.orientation == -90) {
+		setTimeout(() => {
+			alert('为了更好的体验效果，请把屏幕竖过来');
+		}, 800);
+	}
+}
+window.addEventListener('onorientationchange' in window ? 'orientationchange' : 'resize', screen, false);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  // router,
-  components: { App },
-  template: '<App/>'
-})
+	el: '#app',
+	// router,
+	components: { App },
+	template: '<App/>'
+});
