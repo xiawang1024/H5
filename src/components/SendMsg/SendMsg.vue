efb9636fb674342903189615d19bc131b4deda41
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2018-06-29 17:07:59 +0800

    安卓适配flex

--- src/components/SendMsg/SendMsg.vue
+++ src/components/SendMsg/SendMsg.vue
@@ -150,8 +150,8 @@ export default {
   }
 
   .ipt {
-    flex: 1;
-    height: 60px;
+    flex: 0 0 360px;
+    width: 360px;
     border: 1px solid #0081dc;
     padding-left: 16px;
     border-radius: 12px;
