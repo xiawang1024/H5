8ac84a27b38c30df370dce83939259d1123c10f0
wangxia/work <xiawang1024@icloud.com>
2019-02-02 16:15:25 +0800

    url

--- src/components/Home/data7.js
+++ src/components/Home/data7.js
@@ -3,6 +3,6 @@ const data = {
   title: '三、系列报道',
   subTitle: '新春走基层',
   top: '7',
-  imgObj: { src: `${dirUrl}7.png`, url: 'http://www.hndt.com/131/index.html?id=2375604' }
+  imgObj: { src: `${dirUrl}7.png`, url: '' }
 }
 module.exports = data
